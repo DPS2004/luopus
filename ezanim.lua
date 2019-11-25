1ce1523e68c5e2a88eea0a54e3b845a955be8adb
DPS2004 <iheartscratch@gmail.com>
2019-11-25 08:04:15 -0500

    update ezanim.lua

--- ezanim.lua
+++ ezanim.lua
@@ -1,35 +1,77 @@
 local ezanim = {}
-function ezanim.newanim(png,w,s,b)
-  local a = {}
-  a.w = w
-  a.f = 1
-  a.s = s
-  a.b = b or 0
-  a.img = love.graphics.newImage(png)
-  a.h = a.img:getHeight()
-  a.frames = a.img:getWidth()/(w+a.b)
-  a.quads = {}
+function ezanim.newtemplate(png,w,s,b)
+  local t = {}
+  t.w = w
+
+  t.s = s
+  t.b = b or 0
+  t.img = love.graphics.newImage(png)
+  t.h = t.img:getHeight()
+  t.frames = t.img:getWidth()/(w+t.b)
+  t.quads = {}
   offset = 0
-  for i=0,a.frames - 1 do
+  for i=0,t.frames - 1 do
     
-    quad = love.graphics.newQuad(i * a.w + offset, 0 , a.w, a.h, a.img:getWidth(), a.img:getHeight())
-    table.insert(a.quads, quad)
-    offset = offset + a.b
+    quad = love.graphics.newQuad(i * t.w + offset, 0 , t.w, t.h, t.img:getWidth(), t.img:getHeight())
+    table.insert(t.quads, quad)
+    offset = offset + t.b
   end
+
+  return t
+end
+function ezanim.newanim(temp)
+  local a = {}
+  a.temp = temp
+  a.f = 1
   a.time = 0
   return a
 end
 function ezanim.animupdate(a)
   a.time = a.time + love.timer.getDelta()*60
-  if a.time >= a.s then
-    framesmissed = math.floor(a.time / a.s)
+  if a.time >= a.temp.s then
+    framesmissed = math.floor(a.time / a.temp.s)
     a.f = a.f + framesmissed
-    a.time = a.time - framesmissed * a.s
+    a.time = a.time - framesmissed * a.temp.s
     
   end
-  while a.f >= a.frames + 1 do
-    a.f = a.f - a.frames
+  while a.f >= a.temp.frames + 1 do
+    a.f = a.f - a.temp.frames
   end 
+end
+function ezanim.outlinedraw(a,x,y,t,c1,c2,r,sx,sy,ox,oy,kx,ky)
+
+  x = x or 0
+  y = y or 0
+  c1 = c1 or {0,0,0,1}
+  c2 = c2 or {1,1,1,1}
+  t = t or false
+
+  r = r or 0
+  sx = sx or 1
+  sy = sy or sx
+  ox = ox or 0
+  oy = oy or 0
+  kx = kx or 0
+  ky = ky or 0
+  quad = a.temp.quads[a.f]
+  love.graphics.setColor(c1)
+  if t then
+    for i1=-1,1 do
+      for i2=-1,1 do
+        love.graphics.draw(a.temp.img,quad,x+i1,y+i2,r,sx,sy,ox,oy,kx,ky)
+      end
+    end
+  else
+    love.graphics.draw(a.temp.img,quad,x+1,y,r,sx,sy,ox,oy,kx,ky)
+    love.graphics.draw(a.temp.img,quad,x-1,y,r,sx,sy,ox,oy,kx,ky)
+    love.graphics.draw(a.temp.img,quad,x,y+1,r,sx,sy,ox,oy,kx,ky)
+    love.graphics.draw(a.temp.img,quad,x,y-1,r,sx,sy,ox,oy,kx,ky)
+  end
+  love.graphics.setColor(c2)
+
+  love.graphics.draw(a.temp.img,quad,x,y,r,sx,sy,ox,oy,kx,ky)
+  love.graphics.setColor(1,1,1,1)
+
 end
 function ezanim.animdraw(a,x,y,r,sx,sy,ox,oy,kx,ky)
   x = x or 0
@@ -41,8 +83,8 @@ function ezanim.animdraw(a,x,y,r,sx,sy,ox,oy,kx,ky)
   oy = oy or 0
   kx = kx or 0
   ky = ky or 0
-  quad = a.quads[a.f]
-  love.graphics.draw(a.img,quad,x,y,r,sx,sy,ox,oy,kx,ky)
+  quad = a.temp.quads[a.f]
+  love.graphics.draw(a.temp.img,quad,x,y,r,sx,sy,ox,oy,kx,ky)
 end
 function ezanim.resetanim(a)
   a.f=1
